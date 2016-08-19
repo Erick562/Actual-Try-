69b09542b674ed7a090c1aade9fbbbe74e32fe69
Udemy Course <gracianerick@yahoo.com>
2016-08-19 14:48:47 -0700

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base 
+	validates :username, presence: true, uniqueness: {case_sensitive: false} , length: {minimum: 4, maximum: 20}
+	VALID_EMAIL_REGEX = /\A[\w+\-]+@[a-z\d\-_]+\.[a-z]+\z/i
+	validates :email, presence: true, uniqueness: {case_sensitive: true}, length: {minimum: 5, maximum: 20}, format: {with: VALID_EMAIL_REGEX}
 end 
+
+#oook so it appears that the hashes are used when you want to add attributes to a statement , get it bro?
